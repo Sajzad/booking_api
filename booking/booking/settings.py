35b66fbb8cee7df6fd3d42aefb576f5980630f9d
Sajzad <sajzadhasan@gmail.com>
2021-11-25 23:30:24 +0600

    added

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bv!9kfxf_a+!#*zb19w7la8h@f4ucbg_p7m0xk1x26s#p+0b8a'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
